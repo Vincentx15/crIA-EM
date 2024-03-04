Small fixes in the computation (such as comparing only fvs GT in dockim outputs)

paper/dockim_predict_test.py
@@ -217,7 +217,7 @@ def get_hit_rates_dockim(nano=False, test_path="../data/testset/"):
     time_init = time.time()
     all_res = {}
     with pymol2.PyMOL() as p:
-        for step, ((pdb, mrc, resolution), selections) in enumerate(pdb_selections.items()):
+        for step, ((pdb, mrc, resolution), selections) in enumerate(sorted(pdb_selections.items())):
             # if not pdb == '8SB5':
             #     continue
             if not step % 20:
@@ -229,7 +229,7 @@ def get_hit_rates_dockim(nano=False, test_path="../data/testset/"):
             gt_com = []
             for i in range(len(selections)):
                 # We use the Fv GT in the vase of Fabs
-                gt_name = os.path.join(pdb_dir, f'gt_{"nano_" if nano else "fv_"}{i}.pdb')
+                gt_name = os.path.join(pdb_dir, f'gt_{"nano_" if nano else ""}{i}.pdb')
                 p.cmd.load(gt_name, 'gt')
                 gt_coords = p.cmd.get_coords('gt')
                 com = np.mean(gt_coords, axis=0)
@@ -282,10 +282,9 @@ if __name__ == '__main__':
     for sorted_split in [True, False]:
         test_path = f'../data/testset{"" if sorted_split else "_random"}'
         for nano in [False, True]:
-            # for nano in [True]:
             csv_in = f'../data/{"nano_" if nano else ""}csvs/{"sorted_" if sorted_split else ""}filtered_test.csv'
             print('Getting data for ', string_rep(sorted_split=sorted_split, nano=nano))
-            # get_systems(csv_in=csv_in, nano=nano, test_path=test_path)
+            get_systems(csv_in=csv_in, nano=nano, test_path=test_path)
 
     # Doing all at the same times avoid side effects
     inputs_dockim = []
@@ -298,7 +297,7 @@ if __name__ == '__main__':
     for sorted_split in [True, False]:
         test_path = f'../data/testset{"" if sorted_split else "_random"}'
         for nano in [False, True]:
-            print('Getting hit rates for :', string_rep(nano=nano))
+            print('Getting hit rates for :', string_rep(nano=nano, sorted_split=sorted_split))
             get_hit_rates_dockim(nano=nano, test_path=test_path)
 
     # Quick runtime computation for dockim. This is a pessimistic estimate,

paper/pr_curve.py
@@ -71,17 +71,12 @@ def compute_hr(nano=False, test_path='../data/testset', num_setting=False, use_m
         if overpreds > 0 and underpreds > 0:
             print(pdb, 'winner !')
         all_hr[pdb] = (errors, num_gt)
-        # if errors > 0:
-        #     print(f'For PDB {pdb}, {"overprediction" if overpreds > 0 else "underprediction"} :'
-        #           f'predicted num: {num_pred}, GT num {num_gt}, all hits{hits_thresh}')
     print('Overpredictions : ', sum([x[1] for x in overpreds_list]), len(overpreds_list), overpreds_list)
     print('Underpredictions : ', sum([x[1] for x in underpreds_list]), len(underpreds_list), underpreds_list)
 
     hit_rate_sys = np.mean([100 * (1 - errors / num_gt) for errors, num_gt in all_hr.values()])
     hit_rate_ab = 100 * (1 - np.sum([errors for errors, _ in all_hr.values()]) / np.sum(
         [num_gt for _, num_gt in all_hr.values()]))
-    # print('sys : ',hit_rate_sys)
-    # print('ab : ', hit_rate_ab)
     print(f"{hit_rate_sys:.1f}")
     print(f"{hit_rate_ab:.1f}")
 
@@ -96,8 +91,14 @@ def plot_pr_curve(nano=False, test_path='../data/testset', use_mixed_model=True,
     all_gt = []
     all_preds = []
     for pdb, (gt_hits_thresh, hits_thresh, resolution) in all_res.items():
-        if pdb == '7YC5':
-            continue
+        if test_path == '../data/testset':
+            # Misclassified nano in sorted
+            if pdb == '7YC5':
+                continue
+        if test_path == '../data/testset_random':
+            # Systems containing both Fab and nAb in random split
+            if pdb in ['7PIJ', '7SK5', '7WPD', '7XOD', '7ZLJ', '8HIK']:
+                continue
         gt_hits_thresh = np.array(gt_hits_thresh)
         hits_thresh = np.array(hits_thresh)
         num_gt = np.max(gt_hits_thresh)
@@ -105,8 +106,8 @@ def plot_pr_curve(nano=False, test_path='../data/testset', use_mixed_model=True,
         all_precisions.append(precision)
         all_gt.append(gt_hits_thresh / num_gt)
         all_preds.append(hits_thresh / num_gt)
-        if precision[-1] < 0.9:
-            print(pdb, num_gt, hits_thresh)
+        # if precision[-1] < 0.9:
+        #     print(pdb, num_gt, hits_thresh)
     all_precisions = np.stack(all_precisions)
     all_precisions_mean = np.mean(all_precisions, axis=0)
     all_precisions_std = np.std(all_precisions, axis=0)
@@ -135,10 +136,10 @@ def plot_pr_curve(nano=False, test_path='../data/testset', use_mixed_model=True,
         ax.plot(x, mean, **kwargs)
         ax.fill_between(x, mean + std, mean - std, alpha=0.5)
 
-    # plot_in_between(ax, all_precisions_mean, all_precisions_std,
-    #                 label=rf'Fractional precision{"_fab" if not use_mixed_model else ""}')
-    plot_in_between(ax, all_gt_mean, all_gt_std, label=r'\texttt{Ground Truth}')
-    plot_in_between(ax, all_preds_mean, all_preds_std, label=r'\texttt{CrAI}')
+    plot_in_between(ax, all_precisions_mean, all_precisions_std,
+                    label=rf'Fractional precision{"_fab" if not use_mixed_model else ""}')
+    # plot_in_between(ax, all_gt_mean, all_gt_std, label=r'\texttt{Ground Truth}')
+    # plot_in_between(ax, all_preds_mean, all_preds_std, label=r'\texttt{CrAI}')
 
     plt.xlim((1, 10))
     plt.ylim((0., 1))
@@ -154,10 +155,10 @@ if __name__ == '__main__':
     # plot_pr_curve(nano=True)
     # plt.show()
 
-    # plot_pr_curve(nano=False, use_mixed_model=True, dockim=True)
-    # plot_pr_curve(nano=False)
-    # plt.show()
-
+    # plot_pr_curve(nano=False, dockim=True)
+    plot_pr_curve(nano=True)
+    plt.show()
+    oi
     # Compute the # systems with both Fabs and nanobodies as they bug the validation a bit
     # for sorted_split in [True, False]:
     #     test_path = f'../data/testset{"" if sorted_split else "_random"}'
@@ -172,17 +173,23 @@ if __name__ == '__main__':
     #           'Num in both:', len(nab_pdb.intersection(fab_pdb)))
     #     print(sorted(nab_pdb.intersection(fab_pdb)))
 
-    for sorted_split in [True]:
-        # for sorted_split in [True, False]:
+    # # TO PLOT ONE
+    # test_path = f'../data/testset'
+    # # test_path = f'../data/testset_random'
+    # compute_hr(test_path=test_path, nano=True, use_mixed_model=True, num_setting=False)
+    # # compute_hr(test_path=test_path, nano=True, use_mixed_model=True, num_setting=True, dockim=True)
+
+    for sorted_split in [True, False]:
         test_path = f'../data/testset{"" if sorted_split else "_random"}'
-        # for nano in [False, True]:
-        for nano in [False]:
+        for nano in [False, True]:
+            # for nano in [False]:
             print('Results HR for :', string_rep(sorted_split=sorted_split,
                                                  nano=nano,
                                                  dockim=True))
             compute_hr(test_path=test_path, nano=nano, num_setting=True, dockim=True)
-            # for mixed in [False, True]:
-            for mixed in [True]:
+            for mixed in [False, True]:
+                # for mixed in [True]:
+                # no specific nano model
                 if nano and not mixed:
                     continue
                 # for num_setting in [False]: