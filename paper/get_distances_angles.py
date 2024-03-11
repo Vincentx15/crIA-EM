d75f30fad030c9ab1690452a82ec9a718e471620
Vincentx15 <vincent.mallet96@gmail.com>
2024-03-11 22:15:07 +0100

    clean hist

--- paper/get_distances_angles.py
+++ paper/get_distances_angles.py
@@ -3,8 +3,9 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import pickle
-import pymol2
+import seaborn as sns
 import scipy
 import time
 
@@ -36,7 +37,7 @@ def match_transforms_to_angles_dist(gt_transforms, pred_transforms):
         u2 = u2 / np.linalg.norm(u2)
         return np.arccos(np.dot(u1, u2)) * 180 / 3.14
 
-    angles = [get_angle(pred_rz[i], gt_rz[col_ind[i]]) for i in row_ind]
+    angles = [get_angle(pred_rz[row], gt_rz[col]) for row, col in zip(row_ind, col_ind)]
     return position_dists, angles
 
 
@@ -102,39 +103,32 @@ def get_angles_dist(nano=False, test_path="../data/testset/", num_setting=False,
         num_pred_all = pickle.load(open(num_pred_path, 'rb'))
 
     for step, ((pdb, mrc, resolution), selections) in enumerate(sorted(pdb_selections.items())):
-        # if not pdb=='7FAE':
-        #     continue
         if not step % 20:
             print(f"Done {step} / {len(pdb_selections)} in {time.time() - time_init}")
         pdb_dir = os.path.join(test_path, f'{pdb}_{mrc}')
 
-        try:
-            # First get GT transforms
-            gt_name = os.path.join(pdb_dir, f'{pdb}.cif')
-            gt_transforms = pdbsel_to_transforms(gt_name, antibody_selections=selections, cache=False)
+        gt_name = os.path.join(pdb_dir, f'{pdb}.cif')
+        gt_transforms = pdbsel_to_transforms(gt_name, antibody_selections=selections, cache=False)
 
-            if num_setting:
-                num_pred = len(selections)
-            else:
-                num_pred = min(num_pred_all[pdb], 9)
+        if num_setting:
+            num_pred = len(selections)
+        else:
+            num_pred = min(num_pred_all[pdb], 9)
 
-            # Now get the (sorted) list of predicted com
-            pred_transforms = []
-            for i in range(num_pred):
-                pred_name = os.path.join(pdb_dir, f'crai_pred{suffix}{"_nano" if nano else ""}_{i}.pdb')
-                if not os.path.exists(pred_name):
-                    continue
-                dummy_sel = "polymer.protein and polymer.protein"
-                i_th_transform = pdbsel_to_transforms(pred_name, antibody_selections=dummy_sel, cache=False)
-                pred_transforms.extend(i_th_transform)
-            if len(pred_transforms) == 0:
-                all_res[pdb] = None
+        # Now get the (sorted) list of predicted com
+        pred_transforms = []
+        for i in range(num_pred):
+            pred_name = os.path.join(pdb_dir, f'crai_pred{suffix}{"_nano" if nano else ""}_{i}.pdb')
+            if not os.path.exists(pred_name):
                 continue
-            dists, angles = match_transforms_to_angles_dist(gt_transforms, pred_transforms)
-            all_res[pdb] = dists, angles
-        except Exception as e:
-            print("failed on pdb : ", pdb)
+            dummy_sel = "polymer.protein and polymer.protein"
+            i_th_transform = pdbsel_to_transforms(pred_name, antibody_selections=dummy_sel, cache=False)
+            pred_transforms.extend(i_th_transform)
+        if len(pred_transforms) == 0:
             all_res[pdb] = None
+            continue
+        dists, angles = match_transforms_to_angles_dist(gt_transforms, pred_transforms)
+        all_res[pdb] = dists, angles
     pickle.dump(all_res, open(outname_results, 'wb'))
 
 
@@ -176,7 +170,7 @@ def get_distances_all():
                 get_distance_one(nano=nano, test_path=test_path, num_setting=num_setting)
 
 
-def plot_one(test_path=None, dockim=False, nano=False, num_setting=False, suffix='', thresh=10):
+def get_angles_one(test_path=None, dockim=False, nano=False, num_setting=False, suffix='', thresh=10):
     if dockim:
         pickle_name_to_get = f'dockim_angledist{"_nano" if nano else ""}.p'
     else:
@@ -193,47 +187,101 @@ def plot_one(test_path=None, dockim=False, nano=False, num_setting=False, suffix
     return sel_angles
 
 
+def plot_dict_hist(angle_resdict):
+    """
+    Expected to receive keys as legend and values as lists of values
+    :return:
+    """
+    # Rest of the plot decorations
+    plt.rcParams.update({'font.size': 14})
+    plt.rcParams['text.usetex'] = True
+    plt.rc('grid', color='grey', alpha=0.2)
+
+    all_angles = []
+    all_names = []
+    for label, angles in angle_resdict.items():
+        all_angles.extend(angles)
+        all_names.extend([label for _ in range(len(angles))])
+    all_angles = [angle + 1 for angle in all_angles]
+    # all_angles = [int(angle) for angle in all_angles]
+    # print(all_angles)
+    df = pd.DataFrame({'angles': all_angles,
+                       'Model Name': all_names})
+
+    use_log = True
+    if use_log:
+        bins = np.logspace(1, np.log(180) / np.log(10), 10)
+    else:
+        bins = np.linspace(0, 180, 10)
+
+    # print(bins)
+    hist_kwargs = {
+        "multiple": "dodge",
+        "stat": "percent",
+        "kde": False,
+        "bins": bins,
+        "common_norm": False,
+        # "log_scale": (True, True), # does not work with bins
+    }
+    sns.histplot(data=df, x='angles', hue='Model Name', shrink=.9, **hist_kwargs)
+    if use_log:
+        ax = plt.gca()
+        ax.set_xticks = bins
+        ax.xticks = bins
+        ax.set(xscale='log')
+        ax.set(yscale='log')
+    # sns.histplot(all_angles[0], label=labels[0], **hist_kwargs)
+    # sns.histplot(all_angles[1], label=labels[1], **hist_kwargs)
+    # sns.histplot(all_angles[2], label=labels[2], **hist_kwargs)
+    # plt.hist(all_angles[0], label=labels[0], **hist_kwargs)
+    # plt.hist(all_angles[1], label=labels[1], **hist_kwargs)
+    # plt.hist(all_angles[2], label=labels[2], **hist_kwargs)
+    # bw_method = 1
+    # sns.kdeplot(all_angles[0], label=labels[0], clip=(0, 180), bw_method=bw_method)
+    # sns.kdeplot(all_angles[1], label=labels[1], clip=(0, 180), bw_method=bw_method)
+    # sns.kdeplot(all_angles[2], label=labels[2], clip=(0, 180), bw_method=bw_method)
+    # sns.histplot(all_angles, kde=True, label=labels, bins=np.linspace(0, 180, 19))
+    plt.grid(True)
+    plt.xlabel(rf'Angle difference ($^\circ$)')
+    plt.ylabel(rf'Percent')
+    plt.savefig(f'../fig_paper/python/angles.pdf')
+    plt.show()
+
+
 def plot_all():
     test_path = f'../data/testset_random'
     results = {}
     get_angles_dist(nano=False, test_path=test_path, num_setting=False)
-    results['normal'] = plot_one(nano=False, test_path=test_path, num_setting=False)
+    results[r'Fab'] = get_angles_one(nano=False, test_path=test_path, num_setting=False)
 
     get_angles_dist(nano=True, test_path=test_path, num_setting=False)
-    results['nano'] = plot_one(nano=True, test_path=test_path, num_setting=False)
+    results['nAb'] = get_angles_one(nano=True, test_path=test_path, num_setting=False)
 
-    get_angles_dist(nano=False, test_path=test_path, num_setting=False, suffix='_uy', recompute=True)
-    results['uy'] = plot_one(nano=False, test_path=test_path, num_setting=False, suffix='_uy')
-
-    for key, val in results.items():
-        plt.hist(val, label=key, alpha=0.8)
-    plt.legend()
-    plt.show()
+    get_angles_dist(nano=False, test_path=test_path, num_setting=False, suffix='_uy')
+    results['$\overrightarrow{u_y}$'] = get_angles_one(nano=False, test_path=test_path, num_setting=False, suffix='_uy')
+    plot_dict_hist(results)
 
 
 def plot_ablation():
     test_path = f'../data/testset_random'
     results = {}
     get_angles_dist(nano=False, test_path=test_path, num_setting=False)
-    results['normal'] = plot_one(nano=False, test_path=test_path, num_setting=False)
+    results['normal'] = get_angles_one(nano=False, test_path=test_path, num_setting=False)
 
     get_angles_dist(nano=False, test_path=test_path, num_setting=False, suffix='_no_ot')
-    results['no_ot'] = plot_one(nano=False, test_path=test_path, num_setting=False, suffix='_no_ot')
+    results['no_ot'] = get_angles_one(nano=False, test_path=test_path, num_setting=False, suffix='_no_ot')
 
     get_angles_dist(nano=False, test_path=test_path, num_setting=False, suffix='_no_pd')
-    results['no_pd'] = plot_one(nano=False, test_path=test_path, num_setting=False, suffix='_no_pd')
+    results['no_pd'] = get_angles_one(nano=False, test_path=test_path, num_setting=False, suffix='_no_pd')
 
     get_angles_dist(nano=False, test_path=test_path, num_setting=False, suffix='_uy')
-    results['uy'] = plot_one(nano=False, test_path=test_path, num_setting=False, suffix='_uy')
-
-    for key, val in results.items():
-        plt.hist(val, label=key, alpha=0.8)
-    plt.legend()
-    plt.show()
+    results['uy'] = get_angles_one(nano=False, test_path=test_path, num_setting=False, suffix='_uy')
+    plot_dict_hist(results)
 
 
 if __name__ == '__main__':
-    # get_results()
+    pass
+    # get_angledist_results()
     # get_distances_all()
     plot_all()
     # plot_ablation()
